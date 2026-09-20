6d345bc05cf24da27aa307204b33f84d505cccbe
Zolex <israelbrunogd@gmail.com>
2026-09-20 16:32:12 -0300

    optimized formula

--- My first PRNG.py
+++ My first PRNG.py
@@ -4,5 +4,5 @@ number = int(input('Type some random numbers: '))
 set_int_max_str_digits(0)
 rng = number
 for i in range(10):
-    rng = ((rng ** 4 + 782) // 56 - 139 % 8765 * 2) // 23 % 99999999999999
+    rng = ((rng ** 4 + 782) // 56 - 278) // 23 % 10000000000
 print(rng)
